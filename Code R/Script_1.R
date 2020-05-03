274b79554bcb709b75c14df13475d9b0ffc7ddf9
jpguerreroo <jguerror41@gmail.com>
2020-05-03 17:47:48 +0200

    First commit

---
+++ Code R/Script_1.R
@@ -0,0 +1,11 @@
+# 1.- Construye la matriz siguiente de dos formas distintas
+A <- matrix(c(7, 18, -2, 22, -16, 3, 55, 1, 7, -4, 0, 31), ncol = 4)
+A
+
+#Crear una lista
+lista <- list(f1, f2, f3)
+lista
+
+# Array 1
+array1 <- array(1:24, dim=c(4,3,2))
+array1
